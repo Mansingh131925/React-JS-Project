9f82d6da18e70fed1d42793f12c30dca08eba0ac
Mansingh131925 <139048508+Mansingh131925@users.noreply.github.com>
2023-09-21 22:45:45 +0530

    Add files via upload
    reactjs project

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+//  import Sighup from './Componant-B/Pages/SighUp/Sighup';
+// import Restorent from './Componant/Basics/Resturant';
+//  import Layout from './layout';
+// import Hooks from './Componant-A/Hooks/Index';
+   import Index from './Componant-B/Layout/Index';
+// import NavLogin from './Componant-B/Pages/Login/Login';
+// import Login from './Componant-B/Pages/Login/Login.jsx';
+// import LoginMain from './Componant-B/Pages/Login/Login';
+// import Logn_Nav from './Componant-B/Pages/Login/Logn_Nav';
+import IndexC from './Componant-C/IndexC';
+
+
+function App() {
+  return (
+    <>
+     {/* /*componant C start */ }
+{/* <IndexC/> */}
+
+  {/* <Logn_Nav/> */}
+  
+     {/* <Sighup/>  */}
+    {/* <LoginMain/>  */}
+    * <Index/> 
+       {/* <Restorent/> */}
+      {/* <Layout/>
+      <Hooks/> */}
+    
+      
+    </>
+   
+  );
+}
+
+export default App;
